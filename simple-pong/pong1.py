62df0a6675d77fcb8338ff512903b198137790cb
Jethet <henriette.hettinga@protonmail.com>
2022-05-01 18:35:28 +0200

    First setup pong game

---
+++ simple-pong/pong1.py
@@ -0,0 +1,23 @@
+import turtle
+
+win = turtle.getscreen()
+
+# win = turtle.Screen
+win.title("Simple Pong Game")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)  #stops window from updating, speeds up the game
+
+# Player A
+player_a = turtle.Turtle()
+
+# Player B
+
+# Ball
+
+
+# Main loop
+while True:
+  win.update()
+  
+
