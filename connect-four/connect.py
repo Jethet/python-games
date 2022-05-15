9cfa2230eaf26b0eaf91b290dabc3edeb085ea78
Jethet <henriette.hettinga@protonmail.com>
2022-05-15 14:38:19 +0200

    Setup board, setup players

--- connect-four/connect.py
+++ connect-four/connect.py
@@ -5,4 +5,17 @@ def create_board():
   return board
 
 board = create_board()
+
+game_over = False
+turn = 0
+
+while not game_over:
+  # ask player 1 input
+  if turn == 0:
+    selection = int(input("Player 1 make your selection (0-6): "))
+    print(selection)
+    print(type(selection))
+  
+  # ask player 2 input
+
 print(board)
