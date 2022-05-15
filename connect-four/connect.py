522bbc6e1890e0e483f8ee26cc3c56a5d032d2b8
Jethet <henriette.hettinga@protonmail.com>
2022-05-15 14:54:40 +0200

    Fix error with alternating players

--- connect-four/connect.py
+++ connect-four/connect.py
@@ -5,6 +5,7 @@ def create_board():
   return board
 
 board = create_board()
+print(board)
 
 game_over = False
 turn = 0
@@ -13,9 +14,13 @@ while not game_over:
   # ask player 1 input
   if turn == 0:
     selection = int(input("Player 1 make your selection (0-6): "))
-    print(selection)
-    print(type(selection))
+    # print(selection)
+    # print(type(selection))
   
   # ask player 2 input
+  else:
+    selection = int(input("Player 2 make your selection (0-6): "))
 
-print(board)
+  # to alternate between 0 and 1: increase turn and use modular to get 0 or 1
+  turn += 1
+  turn = turn % 2
