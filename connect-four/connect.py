aa464753ff1750befad10a6afa411ad54128a729
Jethet <henriette.hettinga@protonmail.com>
2022-05-15 16:08:35 +0200

    Add check for horizontal locations

--- connect-four/connect.py
+++ connect-four/connect.py
@@ -4,7 +4,7 @@ ROW_COUNT = 6
 COLUMN_COUNT = 7
 
 def create_board():
-  board = np.zeros((6, 7))
+  board = np.zeros((ROW_COUNT, COLUMN_COUNT))
   return board
 
 def drop_piece(board, row, col, piece):
@@ -12,7 +12,7 @@ def drop_piece(board, row, col, piece):
 
 def is_valid_location(board, col):
   # board has six rows, 0 to 6, bottom is 0 and top row is 5
-  return board[5][col] == 0
+  return board[ROW_COUNT - 1][col] == 0
 
 def get_next_open_row(board, col):
   for r in range(ROW_COUNT):
@@ -21,6 +21,18 @@ def get_next_open_row(board, col):
 
 def print_board(board):
   print (np.flip(board, 0))   # numpy command to flip top and bottom
+
+def winning_move(board, piece):
+  # check horizontal locations for winning combination
+  for c in range(COLUMN_COUNT - 3):    # you cannot have 4 starting from the last 3
+    for r in range(ROW_COUNT):
+      # check every place from 0 to 4 to see if 4 are connected
+      if board[r][c] == piece and board[r][c + 1] == piece and board[r][c + 2] == piece and board[r][c + 3] == piece:
+        return True
+  
+  # check vertical locations for winning combination
+   
+    
 
 board = create_board()
 print_board(board)
@@ -36,6 +48,10 @@ while not game_over:
     if is_valid_location(board, col):
       row = get_next_open_row(board, col)
       drop_piece(board, row, col, 1)
+      
+      if winning_move(board, 1):
+        print("Player 1 wins!")
+        game_over = True
     # print(selection)
     # print(type(selection))
   
