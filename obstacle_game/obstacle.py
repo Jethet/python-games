5c3034a9537db240680de750896321206c34cd87
Jethet <henriette.hettinga@protonmail.com>
2022-07-26 16:35:57 +0200

    Create obstacle game folder

---
+++ obstacle_game/obstacle.py
@@ -0,0 +1,11 @@
+""" Goals of game:
+- avoid incoming obstacles
+  => player starts on left side of the screen
+  => obstacles enter on the right and move left in straight line
+- player can move right, left, up and down to avoid obstacles
+- player cannot move off the screen
+- game ends when player is hit by obstacle or when user quits
+"""
+import pygame
+pygame.init()
+
