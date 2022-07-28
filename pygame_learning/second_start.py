c844c45b5351cc4218bf9de8b72dd11292c916a3
Jethet <henriette.hettinga@protonmail.com>
2022-07-28 13:02:39 +0200

    Commit before restarting VSC

--- pygame_learning/second_start.py
+++ pygame_learning/second_start.py
@@ -5,18 +5,30 @@ pygame.init()
 win = pygame.display.set_mode((500, 480))
 pygame.display.set_caption("Second attempt at Pygame")
 
-walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'),
-             pygame.image.load('R3.png'), pygame.image.load('R4.png'),
-             pygame.image.load('R5.png'), pygame.image.load('R6.png'),
-             pygame.image.load('R7.png'), pygame.image.load('R8.png'),
-             pygame.image.load('R9.png')]
-walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'),
-            pygame.image.load('L3.png'), pygame.image.load('L4.png'),
-            pygame.image.load('L5.png'), pygame.image.load('L6.png'),
-            pygame.image.load('L7.png'), pygame.image.load('L8.png'),
-            pygame.image.load('L9.png')]
-bg = pygame.image.load('bg.jpg')
-char = pygame.image.load('standing.png')
+walkRight = [
+    pygame.image.load("R1.png"),
+    pygame.image.load("R2.png"),
+    pygame.image.load("R3.png"),
+    pygame.image.load("R4.png"),
+    pygame.image.load("R5.png"),
+    pygame.image.load("R6.png"),
+    pygame.image.load("R7.png"),
+    pygame.image.load("R8.png"),
+    pygame.image.load("R9.png"),
+]
+walkLeft = [
+    pygame.image.load("L1.png"),
+    pygame.image.load("L2.png"),
+    pygame.image.load("L3.png"),
+    pygame.image.load("L4.png"),
+    pygame.image.load("L5.png"),
+    pygame.image.load("L6.png"),
+    pygame.image.load("L7.png"),
+    pygame.image.load("L8.png"),
+    pygame.image.load("L9.png"),
+]
+bg = pygame.image.load("bg.jpg")
+char = pygame.image.load("standing.png")
 
 clock = pygame.time.Clock()
 
@@ -43,10 +55,10 @@ class player(object):
         if not (self.standing):
             # each sprite is used in 3 frames and framerate is 27 per second
             if self.left:
-                win.blit(walkLeft[self.walkCount//3], (self.x, self.y))
+                win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))
                 self.walkCount += 1
             elif self.right:
-                win.blit(walkRight[self.walkCount//3], (self.x, self.y))
+                win.blit(walkRight[self.walkCount // 3], (self.x, self.y))
                 self.walkCount += 1
         else:
             if self.right:
@@ -76,6 +88,7 @@ def redrawGameWindow():
 
 # MAIN LOOP
 small_guy = player(300, 410, 64, 64)
+bullets = []
 running = True
 
 while running:
@@ -85,7 +98,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    for bullet in bullets:
+        if bullet.x < 500 and bullet.x > 0:
+            bullet.x += bullet.speed
+        else:
+            bullets.pop(bullets.index(bullet))
+
     keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_SPACE]:
+        if len[bullets < 5]:
+            bullets.append(
+                projectile(round(small_guy.x + small_guy.width // 2)),
+                (round(small_guy.y + small_guy.height // 2)),
+            )
     if keys[pygame.K_LEFT] and small_guy.x > small_guy.speed:
         small_guy.x -= small_guy.speed
         small_guy.left = True
@@ -101,7 +127,7 @@ while running:
         small_guy.walkCount = 0
     # when the character jumps, it cannot move up and down
     if not (small_guy.isJump):
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_UP]:
             small_guy.isJump = True
             small_guy.right = False
             small_guy.left = False
@@ -113,7 +139,7 @@ while running:
             neg = 1
             if small_guy.jumpCount < 0:
                 neg = -1
-            small_guy.y -= (small_guy.jumpCount ** 2) * 1 * neg
+            small_guy.y -= (small_guy.jumpCount**2) * 1 * neg
             small_guy.jumpCount -= 1
 
         else:
