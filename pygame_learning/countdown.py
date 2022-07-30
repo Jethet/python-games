05a54b72d674e8943ddc6873d614afed12843177
Jethet <henriette.hettinga@protonmail.com>
2022-07-30 16:33:35 +0200

    Delete newline, not needed

--- pygame_learning/countdown.py
+++ pygame_learning/countdown.py
@@ -4,7 +4,7 @@ def countdown(t):
     while t:
         mins, secs = divmod(t, 60)
         timer = "{:02d} : {:02d}".format(mins, secs)
-        print(timer, end="\r")
+        print(timer)
         time.sleep(1)
         t -= 1
     print("It's time!")
