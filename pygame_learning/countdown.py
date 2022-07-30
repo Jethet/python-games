84321db0555de53d4dd672ab442d77cba878c8eb
Jethet <henriette.hettinga@protonmail.com>
2022-07-30 15:41:52 +0200

    Create countdown with seconds

---
+++ pygame_learning/countdown.py
@@ -0,0 +1,16 @@
+import time
+
+def countdown(t):
+
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = "{:02d} : {:02d}".format(mins, secs)
+        print(timer, end = "\r")
+        time.sleep(5)
+        t -= 1
+        
+    print("Time!")
+
+t = input("your time in seconds: ")
+
+countdown(int(t))
