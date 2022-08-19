3a789dc766a99f7d891e22de8a98cf0a8434217e
Jethet <henriette.hettinga@protonmail.com>
2022-08-19 14:57:43 +0200

    Update notes with Tkinter widgets

--- tk_tutorial/tk.py
+++ tk_tutorial/tk.py
@@ -1,4 +1,12 @@
+# import Tkinter module as tk
 import tkinter as tk
 
+# create an instance of the tk.Tk class => this creates the application window
 root = tk.Tk()
+
+# add a component = widget
+
+# call the mainloop() method of the main window object => this keeps the window
+# visible on the screen and keeps it running until you close it (use it as last
+# statement in the programme)
 root.mainloop()
