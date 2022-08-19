b08ae0bbeb6b877781302ad0f1cadf8c13043c0a
Jethet <henriette.hettinga@protonmail.com>
2022-08-19 12:02:46 +0200

    Test with tkinter

--- moves_testing/moves1.py
+++ moves_testing/moves1.py
@@ -1,9 +1,19 @@
 # Move a string from right to left in an array
 
-playerpos = 6
-screen = [1, 2, 3, 4, 5, 6]
+# playerpos = 6
+# screen = [1, 2, 3, 4, 5, 6]
 
-while playerpos > 0:
-    playerpos = playerpos - 1
-    screen[playerpos] = "player"
-    print(screen)
+# while playerpos > 0:
+#     playerpos = playerpos - 1
+#     screen[playerpos] = "player"
+#     print(screen)
+
+# test tkinter
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+root.title("First tkinter test")
+label = Label(root, text = "Hello World!").pack()
+
+root.mainloop()
