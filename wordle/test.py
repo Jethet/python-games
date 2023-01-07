93a496f7f13587cb917064125c0ac950d28b4398
Jethet <henriette.hettinga@protonmail.com>
2023-01-07 12:15:14 +0100

    Test elif loop, no luck

--- wordle/test.py
+++ wordle/test.py
@@ -7,34 +7,39 @@ green = "\033[92m"
 reset = "\033[0m"
 
 
-def randomWord():
-    randWord = random.choice(wordList)
-    return randWord
+# def randomWord():
+#     randWord = random.choice(wordList)
+#     return randWord
 
 
-def game(randWord):
+# def game(randWord):
+
+word = "maker"
+guess = "table"
+
+def game(word):
     guessNumber = 0
 
     while guessNumber < 5:
         guess = input("Please enter your word: ")
 
         for x in guess:
-            if x in randWord:
+            if x in word:
                 indexGuessChar = [i for i, b in enumerate(guess) if b == x]
-                indexWordChar = [i for i, a in enumerate(randWord) if a == x]
+                indexWordChar = [i for i, a in enumerate(word) if a == x]
                 for b in indexGuessChar:
                     for a in indexWordChar:
                         if b == a:
                             print(f"{green}", x)
-                        if b != a:
+                        elif b != a:
                             print(f"{blue}", x)
             else:
                 print(f"{red}", x)
 
         guessNumber += 1
 
-        print(randWord)
+        print(word)
         print(guessNumber)
 
 
-game(randomWord())
+game(word)
