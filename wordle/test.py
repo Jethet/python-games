ea9a2a029c0b58645efd3ba4100ed68f6a981486
Jethet <henriette.hettinga@protonmail.com>
2024-02-11 13:51:32 +0100

    Change iteration characters

--- wordle/test.py
+++ wordle/test.py
@@ -14,7 +14,7 @@ reset = "\033[0m"
 
 # def game(randWord):
 
-word = "maker"
+word = "peter"
 
 def game(word):
     guessNumber = 0
@@ -27,16 +27,22 @@ def game(word):
             print(f"{green}",word, "Correct!")
 
         if guess != word:
+            # go through each character in the guess
             for x in guess:
+                # if the character is in the goal word
                 if x in word:
+                    # find all the positions in the guess at which this character appears
                     indexGuessChar = [i for i, b in enumerate(guess) if b == x]
+                    # find all the positions in the goal word at which this character appears
                     indexWordChar = [i for i, a in enumerate(word) if a == x]
-                    for b in indexGuessChar:
-                        for a in indexWordChar:
-                            if b == a:
+                                                        # baker
+                    for guessIndex in indexGuessChar:   #    3
+                                                        # peter
+                        for wordIndex in indexWordChar: #  1 3
+                            if guessIndex == wordIndex:
                                 print(f"{green}", x)
                             
-                            if b != a and a != "{green}":
+                            if guessIndex != wordIndex and guessIndex not in indexWordChar:
                                 print(f"{blue}", x)
                 else:
                     print(f"{red}", x)
--- wordle/wordle.py
+++ wordle/wordle.py
@@ -23,22 +23,38 @@ def game(word):
             print(f"{green}","Correct! The word is", word)
 
         if guess != word:
-            for x in guess:
-                if x in word:
-                    indexGuessChar = [i for i, b in enumerate(guess) if b == x]
-                    indexWordChar = [i for i, a in enumerate(word) if a == x]
-                    for b in indexGuessChar:
-                        for a in indexWordChar:
-                            if b == a:
-                                print(f"{green}", x)
-                            elif b != a:
-                                print(f"{blue}", x)
+            # go through each character in the guess
+            for guessChar in guess:
+                # if the character guessed is in the word
+                if guessChar in word:
+                    # find all the positions in the guess at which this character appears
+                    guessCharIndices = [
+                        index for index, char in enumerate(guess)
+                        if char == guessChar
+                    ]
+                    # find all the positions in the goal word at which this character appears
+                    wordCharIndices = [
+                        index for index, char in enumerate(word)
+                        if char == guessChar
+                    ]
+                    for guessCharIndex in guessCharIndices:
+                        for wordCharIndex in wordCharIndices:
+                            if guessCharIndex == wordCharIndex:
+                                print(f"{green}", guessChar)
+                            elif guessCharIndex != wordCharIndex:
+                                print(f"{blue}", guessChar)
                 else:
-                    print(f"{red}", x)
+                    print(f"{red}", guessChar)
             guessNumber += 1
         
         if guessNumber == 5:
             print("Game over! The word is", word)
 
 
-game(randomWord())
+from os import getenv
+cheat_word = getenv("WORD")
+if cheat_word:
+    game(cheat_word)
+else:
+    game(randomWord())
+    
